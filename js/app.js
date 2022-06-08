7aa1401e0528c1fbb4302ad7193c334232578cc4
Tylerjay02 <101195311+Tylerjay02@users.noreply.github.com>
2022-06-08 11:42:32 +0100

    Css changes to responsive gallery

--- js/app.js
+++ js/app.js
@@ -1,19 +1,6 @@
 
 const search = new Filter('search', 'data-caption');
 
-  function search () {
-  let input = document.getElementById('search').value
-    input=input.toLowerCase();
-    let x = document.getElementByClassName('img');
-
-  for (i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-         x[i].style.display="none";
-     }
-     else {
-         x[i].style.display="grid";
-     }
- }
-}
+  
 
 baguetteBox.run('.gallery');
